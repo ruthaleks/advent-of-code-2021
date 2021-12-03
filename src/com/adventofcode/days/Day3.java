76fffce03853d8b39c84cb4c0df85d1b9aefc8b3
Ruth Aleksandrauskaite <r.aleksandrauskaite@klarna.com>
2021-12-03 08:16:45 +0100

    Solution day 3

---
+++ src/com/adventofcode/days/Day3.java
@@ -0,0 +1,157 @@
+package com.adventofcode.days;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import java.util.stream.Collectors;
+
+import static com.adventofcode.days.utils.InputReader.readInput;
+
+public class Day3 {
+
+    private final static int INPUT_LENGTH = 12;
+    public static void main(String[] args) {
+        int[][] input = parseInput();
+        System.out.println("Part 1 = " + part1(input));
+        System.out.println("Part 2 = " + part2(input));
+    }
+
+    private static int part1(int[][] input) {
+        List<Integer> gammaRate = calcGammaRate(input);
+        List<Integer> epsilonRate = calcEpsilonRate(gammaRate);
+        return binToDec(gammaRate) * binToDec(epsilonRate);
+    }
+
+    private static int part2(int[][] input) {
+        int o = calcOxygenRate(input);
+        int c = calcC02Scrub(input);
+
+       // System.out.println(o);
+       // System.out.println(c);
+        return o*c;
+    }
+
+    private static int calcC02Scrub(int[][] input) {
+        List<Integer> activeRows = new ArrayList<>(Collections.nCopies(input.length, 1));
+        List<Integer> co2Scrub = new ArrayList<>();
+        for(int col = 0; col < input[0].length; col++) {
+            int numActiveRows = activeRows.stream().mapToInt(Integer::intValue).sum();
+            if (numActiveRows == 1) {
+                co2Scrub.add(input[activeRows.indexOf(1)][col]);
+            } else {
+                int leastCommon = calMostCommon(input, col, activeRows) == 1 ? 0 : 1;
+                //System.out.println("col = " + (col+1) + " leastCommon " + leastCommon);
+                co2Scrub.add(leastCommon);
+                for (int i = 0; i < input.length; i++) {
+                    if(input[i][col] != leastCommon){
+                        activeRows.set(i, 0);
+                    }
+                }
+            }
+
+        }
+       // System.out.println(co2Scrub);
+        return binToDec(co2Scrub);
+    }
+
+    private static int calcOxygenRate(int[][] input) {
+        List<Integer> activeRows = new ArrayList<>(Collections.nCopies(input.length, 1));
+        List<Integer> oxygenGenRate = new ArrayList<>();
+        for(int col = 0; col < input[0].length; col++) {
+            int numActiveRows = activeRows.stream().mapToInt(Integer::intValue).sum();
+            if (numActiveRows == 1) {
+                oxygenGenRate.add(input[activeRows.indexOf(1)][col]);
+            }else {
+                int mostCommon = calMostCommon(input, col, activeRows);
+                //System.out.println("col = " + (col+1) + " mostCommon " + mostCommon);
+                oxygenGenRate.add(mostCommon);
+                for (int i = 0; i < input.length; i++) {
+                    if(input[i][col] != mostCommon){
+                        activeRows.set(i, 0);
+                    }
+                }
+            }
+
+        }
+        return binToDec(oxygenGenRate);
+    }
+
+    private static int calMostCommon(int[][] input, int col, List<Integer> active) {
+        int sum = 0;
+        int numRows = input.length;
+        //System.out.println(active);
+        int numActiveRows = active.stream().mapToInt(Integer::intValue).sum();
+
+        for (int i = 0; i < numRows ; i++) {
+            if (active.get(i) == 1) {
+                sum += input[i][col];
+            }
+            if (sum > numActiveRows / 2) {
+                return 1;
+            }
+        }
+       // System.out.println("sum of ones = " + sum);
+        //System.out.println("sum of zeroes = " + (numActiveRows - sum));
+
+        return sum >= numActiveRows - sum ? 1 : 0;
+    }
+
+    private static List<Integer> calcEpsilonRate(List<Integer> gammaRate) {
+        // inverse of gamma rate
+        List<Integer> epsilonRateBin = new ArrayList<>();
+
+        for(Integer i: gammaRate) {
+            epsilonRateBin.add(i == 0 ? 1 : 0);
+        }
+        //System.out.println(epsilonRateBin);
+        return epsilonRateBin;
+    }
+
+    private static List<Integer>  calcGammaRate(int[][] input) {
+        int numRows = input.length;
+        int numCols = input[0].length;
+        //System.out.println(numCols);
+        //System.out.println(numRows);
+        List<Integer> gammaRateBin = new ArrayList<>(Collections.nCopies(INPUT_LENGTH, 0));
+
+        for (int c = 0; c < numCols ; c++) {
+            int sum = 0;
+            for (int[] ints : input) {
+                sum += ints[c];
+                if (sum > numRows / 2) {
+                    gammaRateBin.set(c, 1);
+                    break;
+                }
+            }
+        }
+        //System.out.println(gammaRateBin);
+        return gammaRateBin;
+    }
+    
+    private static int binToDec(List<Integer> bin) {
+        int dec = 0;
+        for (int i = 0; i < bin.size(); i++) {
+            dec += bin.get(i) * Math.pow(2, bin.size()-i-1);
+            //System.out.println("i = " + i + " bin[i] = " + bin.get(i) + " dec= " + dec);
+        }
+        //System.out.println(dec);
+        return dec;
+    }
+
+    private static int[][] parseInput() {
+        var in = readInput("day3.txt").collect(Collectors.toList());
+        int rows = in.size();
+        int[][] matrix = new int[rows][INPUT_LENGTH];
+        //in.forEach(System.out::println);
+        for (int i = 0; i < rows; i++) {
+            String[] row = in.get(i).split("");
+            for (int j= 0; j < INPUT_LENGTH; j ++) {
+                matrix[i][j] = Integer.parseInt(row[j]);
+            }
+        }
+        //System.out.println(Arrays.deepToString(matrix));
+        return matrix;
+    }
+}
